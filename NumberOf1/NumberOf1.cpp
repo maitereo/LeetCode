019d1211a1a17743b02b9e56680918c9e9674630
yizhou shan <yizhou.shan@zhenrobot.com>
2018-09-13 21:04:08 +0800

    update solution of question <Number of 1>

--- NumberOf1/NumberOf1.cpp
+++ NumberOf1/NumberOf1.cpp
@@ -2,18 +2,38 @@
 #include <bitset>
 #include <stdlib.h>
 
+// class Solution
+// {
+//     public:
+//         int  NumberOf1(int n)
+//         {
+//             int sum = 0, bit = 1;
+//             uint un = n;
+//             while(un!=0)
+//             {
+//                 sum += (int)un&bit;
+//                 un >>= 1;
+//                 std::cout << std::bitset<32>(un) << std::endl;
+//             }
+//             return sum;
+//         }
+// };
+
+
 class Solution
 {
     public:
         int  NumberOf1(int n)
         {
-            int sum = 0, bit = 1;
-            uint un = n;
-            while(un!=0)
+        	// Optimal function
+        	// everytime n-1 will remove the right most
+        	// bit with value '1', this function will
+        	// only loop number of '1' times.
+            int sum = 0;
+            while(n!=0)
             {
-                sum += (int)un&bit;
-                un >>= 1;
-                std::cout << std::bitset<32>(un) << std::endl;
+                sum++;
+                n = (n-1) & n;
             }
             return sum;
         }
